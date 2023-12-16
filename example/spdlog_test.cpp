00282efc54ae378abfea5451fb588cf9123946dd
kaylorchen <kaylor.chen@qq.com>
2023-12-16 21:00:24 +0800

    add some define function

--- example/spdlog_test.cpp
+++ example/spdlog_test.cpp
@@ -1,28 +1,33 @@
 //
 // Created by kaylor on 12/16/23.
 //
-#include "kaylordut/log/logger.h"
 #include "iostream"
+#include "kaylordut/log/logger.h"
 #include "thread"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   int a = 0;
   KAYLORDUT_LOG_TRACE("a = {}", a++);
   KAYLORDUT_LOG_DEBUG("a = {}", a++);
   KAYLORDUT_LOG_INFO("a = {}", a++);
   KAYLORDUT_LOG_WARN("a = {}", a++);
-  auto foo = [&a](){
-    KAYLORDUT_LOG_INFO("a = {}", a++);
-  };
+  auto foo = [&a]() { KAYLORDUT_LOG_INFO("a = {}", a++); };
   foo();
   KAYLORDUT_LOG_ERROR("a = {}", a++);
   KAYLORDUT_LOG_CRITICAL("a = {}", a++);
   std::thread(foo).join();
   int count = 5;
-  do{
-    KAYLORDUT_LOG_DEBUG_ONCE("once");
+  do {
+    KAYLORDUT_LOG_INFO_ONCE("once");
     KAYLORDUT_LOG_DEBUG("normal")
-  }
-  while(count--);
+    KAYLORDUT_LOG_INFO_ONCE("test");
+    KAYLORDUT_LOG_INFO("count is {}", count);
+  } while (--count);
+  KAYLORDUT_LOG_INFO("count is {}", count);
+  KAYLORDUT_LOG_CRITICAL_EXPRESSION(count == 0, "count is {}", count);
+  KAYLORDUT_LOG_CRITICAL_FUNCTION([]() { return true; },
+                                  "return value is true");
+  KAYLORDUT_LOG_CRITICAL_FUNCTION([]() { return false; },
+                                  "return value is false");
   return 0;
 }
--- log/logger.h
+++ log/logger.h
@@ -4,11 +4,12 @@
 
 #pragma once
 #include <fmt/format.h>
+
+#include "iostream"
 #include "map"
-#include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/systemd_sink.h"
-#include "iostream"
+#include "spdlog/spdlog.h"
 
 namespace common {
 
@@ -22,23 +23,26 @@ class Logger {
   Logger(std::string &&logger_name) {
     try {
       std::vector<spdlog::sink_ptr> sinks;
-      if (IsEnableStdOut()){
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+      if (IsEnableStdOut()) {
+        auto console_sink =
+            std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
         sinks.emplace_back(console_sink);
       }
-      if (IsEnableSystemd()){
+      if (IsEnableSystemd()) {
         auto systemd_sink = std::make_shared<spdlog::sinks::systemd_sink_mt>();
         sinks.emplace_back(systemd_sink);
       }
 
-      logger_ = std::make_shared<spdlog::logger>(logger_name, sinks.begin(), sinks.end());
+      logger_ = std::make_shared<spdlog::logger>(logger_name, sinks.begin(),
+                                                 sinks.end());
       logger_->set_level(ReadLogLevelFromEnv());
       logger_->set_pattern("%v");
+    } catch (const spdlog::spdlog_ex &ex) {
     }
-    catch (const spdlog::spdlog_ex &ex) {}
   }
   ~Logger() {}
   const std::shared_ptr<spdlog::logger> GetLogger() const { return logger_; }
+
  private:
   spdlog::level::level_enum ReadLogLevelFromEnv() {
     const char *value = std::getenv("SPDLOG_LEVEL");
@@ -49,19 +53,20 @@ class Logger {
           {"info", spdlog::level::info},
           {"warning", spdlog::level::warn},
           {"error", spdlog::level::err},
-          {"critical", spdlog::level::critical}
-      };
+          {"critical", spdlog::level::critical}};
       try {
         auto ret = level_map.at(std::string(value));
         return ret;
       } catch (const std::out_of_range &e) {
         // 返回默认值
-        std::cout << "environment variable SPDLOG_LEVEL is invalid, and set default level\n";
+        std::cout << "environment variable SPDLOG_LEVEL is invalid, and set "
+                     "default level\n";
         return spdlog::level::info;
       }
     } else {
       // 返回默认值
-      std::cout << "environment variable SPDLOG_LEVEL is not set, and set default level\n";
+      std::cout << "environment variable SPDLOG_LEVEL is not set, and set "
+                   "default level\n";
       return spdlog::level::info;
     }
   }
@@ -73,17 +78,20 @@ class Logger {
     const char *value = std::getenv("SPDLOG_STDOUT");
     if (value != nullptr) {
       std::string var = std::string(value);
-      if (var == "true") { return true; }
-      else if (var == "false") { return false; }
-      else {
-        std::cout << "environment variable SPDLOG_STDOUT is invalid, and set true\n";
+      if (var == "true") {
+        return true;
+      } else if (var == "false") {
+        return false;
+      } else {
+        std::cout
+            << "environment variable SPDLOG_STDOUT is invalid, and set true\n";
         return true;
       }
     }
-    std::cout << "environment variable SPDLOG_STDOUT is not set, and set true\n";
+    std::cout
+        << "environment variable SPDLOG_STDOUT is not set, and set true\n";
     return true;
   }
-
 
   /**
    * 通过环境变量使能systemd的输出
@@ -93,14 +101,18 @@ class Logger {
     const char *value = std::getenv("SPDLOG_SYSTEMD");
     if (value != nullptr) {
       std::string var = std::string(value);
-      if (var == "true") { return true; }
-      else if (var == "false") { return false; }
-      else {
-        std::cout << "environment variable SPDLOG_SYSTEMD is invalid, and set false\n";
+      if (var == "true") {
+        return true;
+      } else if (var == "false") {
+        return false;
+      } else {
+        std::cout << "environment variable SPDLOG_SYSTEMD is invalid, and set "
+                     "false\n";
         return false;
       }
     }
-    std::cout << "environment variable SPDLOG_SYSTEMD is not set, and set false\n";
+    std::cout
+        << "environment variable SPDLOG_SYSTEMD is not set, and set false\n";
     return false;
   }
 
@@ -108,35 +120,184 @@ class Logger {
 };
 }  // namespace common
 
-#define SUFFIX_FORMAT fmt::format("{}() at {}:{}",__func__ ,__FILE__, __LINE__)
-#define DEBUG_FORMAT (fmt::format("| [DEBUG] ") + SUFFIX_FORMAT)
-#define TRACE_FORMAT (fmt::format("| [TRACE] ") + SUFFIX_FORMAT)
-#define INFO_FORMAT (fmt::format("| [INFO] ") + SUFFIX_FORMAT)
-#define WARN_FORMAT (fmt::format("| [WARN] ") + SUFFIX_FORMAT)
-#define ERROR_FORMAT (fmt::format("| [ERROR] ") + SUFFIX_FORMAT)
-#define CRITICAL_FORMAT (fmt::format("| [CRITICAL] ") + SUFFIX_FORMAT)
+#define SUFFIX_FORMAT fmt::format("{}() at {}:{}", __func__, __FILE__, __LINE__)
+#define DEBUG_FORMAT (fmt::format(" | [DEBUG] ") + SUFFIX_FORMAT)
+#define TRACE_FORMAT (fmt::format(" | [TRACE] ") + SUFFIX_FORMAT)
+#define INFO_FORMAT (fmt::format(" | [INFO] ") + SUFFIX_FORMAT)
+#define WARN_FORMAT (fmt::format(" | [WARN] ") + SUFFIX_FORMAT)
+#define ERROR_FORMAT (fmt::format(" | [ERROR] ") + SUFFIX_FORMAT)
+#define CRITICAL_FORMAT (fmt::format(" | [CRITICAL] ") + SUFFIX_FORMAT)
 
-#define KAYLORDUT_LOG_TRACE(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::trace, fmt::format(__VA_ARGS__) + TRACE_FORMAT); }
+#define KAYLORDUT_LOG_TRACE(...)                                 \
+  {                                                              \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),   \
+                       spdlog::level::trace,                     \
+                       fmt::format(__VA_ARGS__) + TRACE_FORMAT); \
+  }
+#define KAYLORDUT_LOG_TRACE_ONCE(...)  \
+  do {                                 \
+    static bool once = true;           \
+    if (once) {                        \
+      once = false;                    \
+      KAYLORDUT_LOG_TRACE(__VA_ARGS__) \
+    }                                  \
+  } while (0)
 
-#define KAYLORDUT_LOG_DEBUG(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::debug, fmt::format(__VA_ARGS__) + DEBUG_FORMAT); }
-#define KAYLORDUT_LOG_DEBUG_ONCE(...) \
-    do{                               \
-       static bool once =true;        \
-       if(once){                      \
-          once = false;               \
-          KAYLORDUT_LOG_DEBUG(__VA_ARGS__)\
-       }\
-    }while(0)
-#define KAYLORDUT_LOG_INFO(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::info, fmt::format(__VA_ARGS__) + INFO_FORMAT); }
+#define KAYLORDUT_LOG_TRACE_FUNCTION(function, ...) \
+  do {                                              \
+    if ((*function)()) {                            \
+      KAYLORDUT_LOG_TRACE(__VA_ARGS__)              \
+    }                                               \
+  } while (0)
 
-#define KAYLORDUT_LOG_WARN(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::warn, fmt::format(__VA_ARGS__) + WARN_FORMAT); }
+#define KAYLORDUT_LOG_TRACE_EXPRESSION(expression, ...) \
+  do {                                                  \
+    if (expression) {                                   \
+      KAYLORDUT_LOG_TRACE(__VA_ARGS__)                  \
+    }                                                   \
+  } while (0)
 
-#define KAYLORDUT_LOG_ERROR(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::err, fmt::format(__VA_ARGS__) + ERROR_FORMAT); }
+#define KAYLORDUT_LOG_DEBUG(...)                                 \
+  {                                                              \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),   \
+                       spdlog::level::debug,                     \
+                       fmt::format(__VA_ARGS__) + DEBUG_FORMAT); \
+  }
+#define KAYLORDUT_LOG_DEBUG_ONCE(...)  \
+  do {                                 \
+    static bool once = true;           \
+    if (once) {                        \
+      once = false;                    \
+      KAYLORDUT_LOG_DEBUG(__VA_ARGS__) \
+    }                                  \
+  } while (0)
 
-#define KAYLORDUT_LOG_CRITICAL(...) \
-    { SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(), spdlog::level::critical, fmt::format(__VA_ARGS__) + CRITICAL_FORMAT); }
+#define KAYLORDUT_LOG_DEBUG_FUNCTION(function, ...) \
+  do {                                              \
+    if ((*function)()) {                            \
+      KAYLORDUT_LOG_DEBUG(__VA_ARGS__)              \
+    }                                               \
+  } while (0)
+
+#define KAYLORDUT_LOG_DEBUG_EXPRESSION(expression, ...) \
+  do {                                                  \
+    if (expression) {                                   \
+      KAYLORDUT_LOG_DEBUG(__VA_ARGS__)                  \
+    }                                                   \
+  } while (0)
+
+#define KAYLORDUT_LOG_INFO(...)                                 \
+  {                                                             \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),  \
+                       spdlog::level::info,                     \
+                       fmt::format(__VA_ARGS__) + INFO_FORMAT); \
+  }
+#define KAYLORDUT_LOG_INFO_ONCE(...)  \
+  do {                                \
+    static bool once = true;          \
+    if (once) {                       \
+      once = false;                   \
+      KAYLORDUT_LOG_INFO(__VA_ARGS__) \
+    }                                 \
+  } while (0)
+
+#define KAYLORDUT_LOG_INFO_FUNCTION(function, ...) \
+  do {                                             \
+    if ((*function)()) {                           \
+      KAYLORDUT_LOG_INFO(__VA_ARGS__)              \
+    }                                              \
+  } while (0)
+
+#define KAYLORDUT_LOG_INFO_EXPRESSION(expression, ...) \
+  do {                                                 \
+    if (expression) {                                  \
+      KAYLORDUT_LOG_INFO(__VA_ARGS__)                  \
+    }                                                  \
+  } while (0)
+
+#define KAYLORDUT_LOG_WARN(...)                                 \
+  {                                                             \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),  \
+                       spdlog::level::warn,                     \
+                       fmt::format(__VA_ARGS__) + WARN_FORMAT); \
+  }
+#define KAYLORDUT_LOG_WARN_ONCE(...)  \
+  do {                                \
+    static bool once = true;          \
+    if (once) {                       \
+      once = false;                   \
+      KAYLORDUT_LOG_WARN(__VA_ARGS__) \
+    }                                 \
+  } while (0)
+
+#define KAYLORDUT_LOG_WARN_FUNCTION(function, ...) \
+  do {                                             \
+    if ((*function)()) {                           \
+      KAYLORDUT_LOG_WARN(__VA_ARGS__)              \
+    }                                              \
+  } while (0)
+
+#define KAYLORDUT_LOG_WARN_EXPRESSION(expression, ...) \
+  do {                                                 \
+    if (expression) {                                  \
+      KAYLORDUT_LOG_WARN(__VA_ARGS__)                  \
+    }                                                  \
+  } while (0)
+
+#define KAYLORDUT_LOG_ERROR(...)                                 \
+  {                                                              \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),   \
+                       spdlog::level::err,                       \
+                       fmt::format(__VA_ARGS__) + ERROR_FORMAT); \
+  }
+#define KAYLORDUT_LOG_ERROR_ONCE(...)  \
+  do {                                 \
+    static bool once = true;           \
+    if (once) {                        \
+      once = false;                    \
+      KAYLORDUT_LOG_ERROR(__VA_ARGS__) \
+    }                                  \
+  } while (0)
+
+#define KAYLORDUT_LOG_ERORR_FUNCTION(function, ...) \
+  do {                                              \
+    if ((*function)()) {                            \
+      KAYLORDUT_LOG_ERORR(__VA_ARGS__)              \
+    }                                               \
+  } while (0)
+
+#define KAYLORDUT_LOG_ERORR_EXPRESSION(expression, ...) \
+  do {                                                  \
+    if (expression) {                                   \
+      KAYLORDUT_LOG_ERORR(__VA_ARGS__)                  \
+    }                                                   \
+  } while (0)
+
+#define KAYLORDUT_LOG_CRITICAL(...)                                 \
+  {                                                                 \
+    SPDLOG_LOGGER_CALL(common::Logger::Instance().GetLogger(),      \
+                       spdlog::level::critical,                     \
+                       fmt::format(__VA_ARGS__) + CRITICAL_FORMAT); \
+  }
+#define KAYLORDUT_LOG_CRITICAL_ONCE(...)  \
+  do {                                    \
+    static bool once = true;              \
+    if (once) {                           \
+      once = false;                       \
+      KAYLORDUT_LOG_CRITICAL(__VA_ARGS__) \
+    }                                     \
+  } while (0)
+
+#define KAYLORDUT_LOG_CRITICAL_FUNCTION(function, ...) \
+  do {                                                 \
+    if ((*function)()) {                               \
+      KAYLORDUT_LOG_CRITICAL(__VA_ARGS__)              \
+    }                                                  \
+  } while (0)
+
+#define KAYLORDUT_LOG_CRITICAL_EXPRESSION(expression, ...) \
+  do {                                                     \
+    if (expression) {                                      \
+      KAYLORDUT_LOG_CRITICAL(__VA_ARGS__)                  \
+    }                                                      \
+  } while (0)
